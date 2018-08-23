b1b14d6d15cd7d291d5350ba360de5a9cfec8106
lex <583036108@qq.com>
2018-08-24 00:10:37 +1200

    pre-processing

--- Wine_quality/Data_preparation.py
+++ Wine_quality/Data_preparation.py
@@ -1,11 +1,35 @@
 import pandas
 
-file_path = "./data/White Wine Dataset.csv"
 
-data = pandas.read_csv(file_path, header=0, sep=',')
+def pre_processing():
+    file_path = "./data/White Wine Dataset.csv"
 
-print(data.head())
+    pandas.set_option("max_columns", 20)
+    data = pandas.read_csv(file_path, header=0, sep=',')
 
-summary = data.describe()
-print(summary)
+    print(data.dtypes)
+    print(data.head())
 
+    summary = data.describe()
+    print(summary)
+
+    std_data = standardization(data, summary)
+    print(std_data)
+
+
+def standardization(data, summary):
+    """
+    :param data
+    :param summary count, mean, standard deviation
+    :return: standized
+    """
+    for i in range(data.shape[1]):
+        mean = summary.iloc[1, i]
+        std = summary.iloc[2, i]
+        data.iloc[:, i] = (data.iloc[:, i] - mean) / std
+
+    return data
+
+
+if __name__ == '__main__':
+    pre_processing()
