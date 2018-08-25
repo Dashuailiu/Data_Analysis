f3eee7b3ad5953c4b384a67e727f9433b9a0da27
lex <583036108@qq.com>
2018-08-25 13:03:10 +1200

    data exploration

--- Wine_quality/data_exploration.py
+++ Wine_quality/data_exploration.py
@@ -1,45 +1,72 @@
-import pandas
-
 from pylab import *
+
+import pandas
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from scipy.stats import kstest, normaltest
 
 from utilities import z_score, z_score_np
 
 
-def data_overall():
+def file_open():
     file_path = "./data/White Wine Dataset.csv"
 
     pandas.set_option("max_columns", 20)
     data = pandas.read_csv(file_path, header=0, sep=',')
 
+    return data
+
+
+def data_overall(data=pandas.DataFrame()):
+    target_col = 'quality'
     print(data.dtypes)
     print(data.head())
 
     summary = data.describe()
     print(summary)
 
-    std_data = z_score(data)
+    std_data = z_score(data, [target_col])
     print(std_data)
 
-    # plt.boxplot(std_data.values)
-    # plt.xlabel("Attributes")
-    # plt.ylabel("Quartile Range - Normalized")
-    # plt.show()
+    # box plot for each attributes
+    plt.boxplot(std_data.values)
+    plt.xlabel("Attributes")
+    plt.ylabel("Quartile Range - Normalized")
+    plt.show()
 
     cmp = plt.cm.RdYlBu
     col_num = len(std_data.columns)-1
     r_num = len(std_data.index)
+    legend_list = list()
+    legend_patch_list = list()
+    target_mean = np.mean(data[target_col])
+    target_std = np.std(data[target_col])
+    std_target_func = lambda x: (x - target_mean)/target_std
+
     for i in range(r_num):
         record = std_data.iloc[i, 1:col_num]
-        label_color = 1.0/(1.0 + exp(-std_data.iloc[i, col_num]))
+        label_color = 1.0/(1.0 + exp(-std_target_func(std_data.iloc[i, col_num])))
+        if std_data.iloc[i, col_num] not in legend_list:
+            legend_patch = mpatches.Patch(color=cmp(label_color), label=std_data.iloc[i, col_num])
+            legend_patch_list.append(legend_patch)
+            legend_list.append(std_data.iloc[i, col_num])
         record.plot(color=cmp(label_color), alpha=0.5, label=std_data.iloc[i, col_num])
 
     plt.xlabel("Attribute Index")
     plt.ylabel("Attribute Values")
-    labels = plt.get_figlabels()
-    plt.legend(labels[::-1])
+    legend_patch_list.sort(key=lambda x: x.get_label())
+    plt.legend(handles=legend_patch_list)
     plt.show()
 
 
+def visualization(func, data=pandas.DataFrame(), columns=None):
+    for col_name in columns:
+        func(data[col_name])
+        plt.xlabel(col_name)
+        plt.show()
+
+
 if __name__ == '__main__':
-    data_overall()
+    data = file_open()
+    visualization(plt.hist, data, data.columns)
+    data_overall(data)
--- utilities/Standardization.py
+++ utilities/Standardization.py
@@ -2,14 +2,27 @@ import pandas
 import numpy
 
 
-def z_score(data):
+def z_score(data, filter_col=None):
     """
     :param data
     :return: standardized
     """
     if not isinstance(data, pandas.DataFrame):
         raise TypeError("The type of input is wrong. Please use pandas.DataFrame.")
-    return (data - data.mean())/data.std()
+
+    if not filter_col:
+        filter_col = []
+
+    summary = data.describe()
+    for i in range(len(data.columns)-1):
+        if data.iloc[:, i].name == filter_col:
+            continue
+        mean = summary.iloc[1, i]
+        std = summary.iloc[2, i]
+        data.iloc[:, i] = (data.iloc[:, i] - mean) / std
+
+    return data
+    # return (data - data.mean())/data.std()
 
 
 def z_score_np(data, ddof=1):
